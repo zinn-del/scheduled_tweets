0113fb83600d41a0d83ba6cdc5dae7ee08b8f0bf
zinn_del <ajetunmobizainab2000@gmail.com>
2023-02-22 09:15:40 +0000

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
  class MainController < ApplicationController
     def index
+     flash.now[:notice] = "logged in sucessfully"
+     flash.now[:alert] = "invalid email or password"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # GET /about
-  get "about-us", to: "about#index",
+  get "about", to: "about#index"
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
